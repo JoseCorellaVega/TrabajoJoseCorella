1b9b4d372100f108957d8d9e0716af82d8e03ceb
JoseCorellaVega <jose.corella.vega@udla.edu.ec>
2025-06-12 17:08:04 -0500

    main

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import jcFigurasGeometricas.CirculoDibujable;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        jcFigurasGeometricas.CirculoDibujable circulo = new CirculoDibujable(100, 0, 0);
+
+        System.out.println("El perimetro del circulo es: " + circulo.getPerimetro());
+        System.out.println("El area del circulo es: " + circulo.getArea());
+
+        JFrame frame = new JFrame("Dibujar circulo");
+        JPanel panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                circulo.dibujar2D(g);
+            }
+
+
+        };
+            panel.setPreferredSize(new Dimension(400, 400));
+            frame.add(panel);
+            frame.pack();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
